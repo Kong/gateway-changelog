59c17263e50ddd876673c38f388478d96f0fc065
Stella Hou <stella.hou@konghq.com>
2026-02-10 15:55:28 +0800

    fix(git): filter unrelated commit for changelog file
    [FTI-7257](https://konghq.atlassian.net/browse/FTI-7257)

--- utils/git.go
+++ utils/git.go
@@ -5,6 +5,19 @@ import (
 	"strings"
 )
 
+// commitActuallyModifiedFile checks if a commit actually modified the file
+// (not just a rename in the history)
+func commitActuallyModifiedFile(workingDir, commit, filename string) bool {
+	// git diff-tree --no-commit-id --name-only -r <commit> -- <filename>
+	cmd := exec.Command("git", "diff-tree", "--no-commit-id", "--name-only", "-r", commit, "--", filename)
+	cmd.Dir = workingDir
+	output, err := cmd.Output()
+	if err != nil {
+		return false
+	}
+	return strings.TrimSpace(string(output)) != ""
+}
+
 func ListCommits(workingDir, filename string) (commits []string, err error) {
 	// execute command git log --follow --pretty=format:%H  -- <path>
 	cmd := exec.Command("git", "log", "--follow", "--pretty=format:%H", "--", filename)
@@ -16,7 +29,10 @@ func ListCommits(workingDir, filename string) (commits []string, err error) {
 
 	lines := strings.Split(string(output), "\n")
 	for _, line := range lines {
-		commits = append(commits, strings.TrimSpace(line))
+		commit := strings.TrimSpace(line)
+		if commit != "" && commitActuallyModifiedFile(workingDir, commit, filename) {
+			commits = append(commits, commit)
+		}
 	}
 	return
 }
