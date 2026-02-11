5202e48956a242f94fa211c9394d02d67ad50c21
Stella Hou <stella.hou@konghq.com>
2026-02-11 15:14:48 +0800

    chore(release): bump changelog to version 2.0.1

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ func New() *cli.App {
 		Name:        "changelog",
 		Usage:       "changelog [command]",
 		Description: "A changelog tool to manage changelog.",
-		Version:     "2.0.0",
+		Version:     "2.0.1",
 
 		// global flags
 		Flags: []cli.Flag{
